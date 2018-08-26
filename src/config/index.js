300cb9304001f31b113d9eec82dcf43f5ffdee8a
xuzhao <x1617301803@gmail.com>
2018-08-27 01:34:24 +0800

    add cross-env to set env

--- src/config/index.js
+++ src/config/index.js
@@ -4,3 +4,4 @@ export const port = process.env.PORT || 5000;
 export const routePrefix = '/api/v1';
 export const connectionString = 'mongodb://140.143.170.28/blog';
 export const blogPath = path.resolve('../blogs');
+export const env = process.env.NODE_ENV;
